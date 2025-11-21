c884d72ddae2df91f94b623334078fc9d589f381
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2025-11-21 10:46:08 -0300

    refactor: organizing the codes and updating the API

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
-from routers import personas
+from personas import router as personas_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    title="PersonaAPI",
-    description="Uma API de Persona 5 Royal",
-    version="1.0"
+    title="Persona 5 Scraper API",
+    version="1.0.0",
+    description="API que faz scrape do Persona 5 Royal Wiki"
 )
 
-app.include_router(personas.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.get("/")
+app.include_router(personas_router)
+
+@app.get("/")
 def root():
-    return {"message": "Bem vindo à PersonaAPI! Acesse /docs para ver a documentação"}
+    return {
+        "message": "API Persona 5 Online",
+        "routes": [
+            "/api/v1/personas",
+            "/api/v1/personas/{nome}",
+            "/api/v1/personas/cache",
+            "/api/v1/personas/cache/{nome}"
+        ]
+    }
