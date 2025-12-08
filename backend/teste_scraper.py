869f76f3fc2dd4491564abc53060fbac2d84e6ef
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2025-12-08 17:29:24 -0300

    test(scraper): changing the shape of the scraper

--- backend/teste_scraper.py
+++ backend/teste_scraper.py
@@ -1,20 +1,24 @@
 import httpx
 import asyncio
+from bs4 import BeautifulSoup
 
-URL = "https://megamitensei.fandom.com/api.php"
+URL = "https://megamitensei.fandom.com/api.php?action=parse&page=List_of_Persona_5_Personas&format=json"
 
 async def test():
-    params = {
-        "action": "parse",
-        "page": "List_of_Persona_5_Personas",
-        "format": "json"
-    }
-
     async with httpx.AsyncClient() as c:
-        r = await c.get(URL, params=params)
+        r = await c.get(URL)
         print("Status:", r.status_code)
+
         data = r.json()
         html = data["parse"]["text"]["*"]
-        print(html[:2000])
 
-asyncio.run(test())
+        soup = BeautifulSoup(html, "html.parser")
+
+        table = soup.find("table")
+        if table:
+            print("Tabela encontrada!")
+            print(str(table)[:2000])
+        else:
+            print("Nenhuma tabela encontrada")
+
+asyncio.run(test()) 
